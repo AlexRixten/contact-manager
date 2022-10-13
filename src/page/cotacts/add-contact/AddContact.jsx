824f479c23a85df1b98c302b9fa48a1f38183623
AlexRixten <rixten.09@gmail.com>
2022-10-14 01:00:15 +0300

    refactor

--- src/page/cotacts/add-contact/AddContact.jsx
+++ src/page/cotacts/add-contact/AddContact.jsx
@@ -1,179 +1,173 @@
-import { queryClient } from "../../../index";
 import { Link, useNavigate } from "react-router-dom";
-// import { ErrorMessage } from '@hookform/error-message';
 import { useForm } from "react-hook-form";
-import { apiRequest } from "../../../api/api";
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../../../index";
+import { createContact } from "../../../api/contacts";
 
-export const AddContact = () => {
-  const navigate = useNavigate();
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    criteriaMode: "all",
-  });
-  const groups = queryClient.getQueryData(["groups"]);
+export function AddContact() {
+    const navigate = useNavigate();
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm({
+        criteriaMode: "all",
+    });
+    const groups = queryClient.getQueryData(["groups"]);
 
-  const createContact = async (data) => {
-    try {
-      const {
-        nameValue,
-        photoUrlValue,
-        mobileValue,
-        emailValue,
-        companyValue,
-        titleValue,
-        groupId,
-      } = data;
-      const response = await apiRequest({
-        type: "POST",
-        url: "/contacts",
-        postData: {
-          company: companyValue,
-          email: emailValue,
-          groupId: groupId,
-          mobile: mobileValue,
-          name: nameValue,
-          photo: photoUrlValue,
-          title: titleValue,
+    const { mutate } = useMutation((values) => createContact(values), {
+        onSuccess: () => {
+            queryClient.invalidateQueries(["contacts"]);
+            navigate("/contacts/list", { replace: true });
         },
-      });
-      if (response) {
-        navigate("/contacts/list", { replace: true });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    });
 
-  return (
-    <>
-      <section className="add-contact p-3">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <p className="h4 text-success fw-bold">Create Contact</p>
-              <p className="fst-italic">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Dolores totam incidunt reprehenderit quas ullam consectetur,
-                obcaecati optio modi perspiciatis earum facere dolorem fugit.
-                Veniam sed beatae commodi corporis, perspiciatis reprehenderit.
-              </p>
+    return (
+        <section className="add-contact p-3">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <p className="h4 text-success fw-bold">
+                            Create Contact
+                        </p>
+                        <p className="fst-italic">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Dolores totam incidunt reprehenderit quas
+                            ullam consectetur, obcaecati optio modi perspiciatis
+                            earum facere dolorem fugit. Veniam sed beatae
+                            commodi corporis, perspiciatis reprehenderit.
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <form onSubmit={handleSubmit(mutate)}>
+                            <div className="mb-2">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Name"
+                                    {...register("nameValue", {
+                                        required: true,
+                                    })}
+                                    aria-invalid={
+                                        errors.nameValue ? "true" : "false"
+                                    }
+                                />
+                                {errors.nameValue?.type === "required" && (
+                                    <p role="alert">Name is required</p>
+                                )}
+                            </div>
+                            <div className="mb-2">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Photo Url"
+                                    {...register("photoUrlValue", {
+                                        required: true,
+                                    })}
+                                    aria-invalid={
+                                        errors.photoUrlValue ? "true" : "false"
+                                    }
+                                />
+                                {errors.photoUrlValue?.type === "required" && (
+                                    <p role="alert">Name is required</p>
+                                )}
+                            </div>
+                            <div className="mb-2">
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Mobile"
+                                    {...register("mobileValue", {
+                                        required: true,
+                                    })}
+                                    aria-invalid={
+                                        errors.mobileValue ? "true" : "false"
+                                    }
+                                />
+                                {errors.mobileValue?.type === "required" && (
+                                    <p role="alert">Name is required</p>
+                                )}
+                            </div>
+                            <div className="mb-2">
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    placeholder="Email"
+                                    {...register("emailValue", {
+                                        required: true,
+                                    })}
+                                    aria-invalid={
+                                        errors.emailValue ? "true" : "false"
+                                    }
+                                />
+                                {errors.emailValue?.type === "required" && (
+                                    <p role="alert">Name is required</p>
+                                )}
+                            </div>
+                            <div className="mb-2">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Company"
+                                    {...register("companyValue", {
+                                        required: true,
+                                    })}
+                                    aria-invalid={
+                                        errors.companyValue ? "true" : "false"
+                                    }
+                                />
+                                {errors.companyValue?.type === "required" && (
+                                    <p role="alert">Name is required</p>
+                                )}
+                            </div>
+                            <div className="mb-2">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Title"
+                                    {...register("titleValue", {
+                                        required: true,
+                                    })}
+                                    aria-invalid={
+                                        errors.titleValue ? "true" : "false"
+                                    }
+                                />
+                                {errors.titleValue?.type === "required" && (
+                                    <p role="alert">Name is required</p>
+                                )}
+                            </div>
+                            <div className="mb-2">
+                                <select
+                                    className="form-control"
+                                    {...register("groupId")}
+                                >
+                                    {/* <option value=''>Select a Group</option> */}
+                                    {groups?.map((item) => (
+                                        <option key={item.id} value={item.id}>
+                                            {item.name}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className="mb-2">
+                                <input
+                                    type="submit"
+                                    className="btn btn-success"
+                                    value="Create"
+                                />
+                                <Link
+                                    to="/contacts/list"
+                                    className="btn btn-dark ms-2"
+                                >
+                                    Cancel
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <form onSubmit={handleSubmit(createContact)}>
-                <div className="mb-2">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Name"
-                    {...register("nameValue", {
-                      required: true,
-                    })}
-                    aria-invalid={errors.nameValue ? "true" : "false"}
-                  />
-                  {errors.nameValue?.type === "required" && (
-                    <p role="alert">Name is required</p>
-                  )}
-                </div>
-                <div className="mb-2">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Photo Url"
-                    {...register("photoUrlValue", {
-                      required: true,
-                    })}
-                    aria-invalid={errors.photoUrlValue ? "true" : "false"}
-                  />
-                  {errors.photoUrlValue?.type === "required" && (
-                    <p role="alert">Name is required</p>
-                  )}
-                </div>
-                <div className="mb-2">
-                  <input
-                    type="number"
-                    className="form-control"
-                    placeholder="Mobile"
-                    {...register("mobileValue", {
-                      required: true,
-                    })}
-                    aria-invalid={errors.mobileValue ? "true" : "false"}
-                  />
-                  {errors.mobileValue?.type === "required" && (
-                    <p role="alert">Name is required</p>
-                  )}
-                </div>
-                <div className="mb-2">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Email"
-                    {...register("emailValue", {
-                      required: true,
-                    })}
-                    aria-invalid={errors.emailValue ? "true" : "false"}
-                  />
-                  {errors.emailValue?.type === "required" && (
-                    <p role="alert">Name is required</p>
-                  )}
-                </div>
-                <div className="mb-2">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Company"
-                    {...register("companyValue", {
-                      required: true,
-                    })}
-                    aria-invalid={errors.companyValue ? "true" : "false"}
-                  />
-                  {errors.companyValue?.type === "required" && (
-                    <p role="alert">Name is required</p>
-                  )}
-                </div>
-                <div className="mb-2">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Title"
-                    {...register("titleValue", {
-                      required: true,
-                    })}
-                    aria-invalid={errors.titleValue ? "true" : "false"}
-                  />
-                  {errors.titleValue?.type === "required" && (
-                    <p role="alert">Name is required</p>
-                  )}
-                </div>
-                <div className="mb-2">
-                  <select className="form-control" {...register("groupId")}>
-                    {/* <option value=''>Select a Group</option> */}
-                    {groups?.map((item) => (
-                      <option key={item.id} value={item.id}>
-                        {item.name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="mb-2">
-                  <input
-                    type="submit"
-                    className="btn btn-success"
-                    value="Create"
-                  />
-                  <Link to={"/contacts/list"} className="btn btn-dark ms-2">
-                    Cancel
-                  </Link>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
-};
+        </section>
+    );
+}
